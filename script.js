2a8dd1c46f8905f1acb6db6029683e66e9be95a3
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-18 11:38:36 +0000

    Commit

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ let myChart = document.getElementById('myChart').getContext('2d');
 
 // Object 
 let popChart = new Chart(myChart, {
-    type: 'bar', // bar, horizontal bar
+    type: 'bar', // bar, horizontal bar, pie, line, donut, radar, polar area
+    data: {
+        labels: ['Barnet', 'Croydon', 'Newham', 'Ealing', 'Enfield'],
+        datasets: []
+    },
+    option: {}
 })
