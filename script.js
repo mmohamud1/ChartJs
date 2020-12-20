6a2ca2df3e21dc1b9c85c600633803e82e3dfec0
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-20 12:48:32 +0000

    styling bar chart in js

--- script.js
+++ script.js
@@ -7,14 +7,7 @@ const pollChart = new Chart(ctx, {
       {
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
         borderColor: [
           'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
@@ -23,7 +16,15 @@ const pollChart = new Chart(ctx, {
           'rgba(153, 102, 255, 1)',
           'rgba(255, 159, 64, 1)',
         ],
-        borderWidth: 1,
+        hoverBackgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderWidth: 5,
       },
     ],
   },
