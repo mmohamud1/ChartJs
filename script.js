05309caf9fdf8c2ec302e6c2c4040067c24dae8c
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-20 13:15:42 +0000

    input poll results

--- script.js
+++ script.js
@@ -2,37 +2,44 @@ const pollData = [
   {
     option: 'Spider-Man',
     votes: 11,
-    color: 'rgb(223, 31, 45)',
+    backgroundColor: 'rgba(223, 31, 45, 0.3)',
+    borderColor: 'rgb(223,31,45)',
   },
   {
     option: 'Superman',
     votes: 8,
-    color: 'rgb(49, 116, 153)',
+    backgroundColor: 'rgba(49, 116, 153, 0.3)',
+    borderColor: 'rgb(49, 116, 153)',
   },
   {
     option: 'Son Goku',
     votes: 5,
-    color: 'rgb(255, 159, 64)',
+    backgroundColor: 'rgba(255, 159, 64, 0.3)',
+    borderColor: 'rgb(255, 159, 64)',
   },
   {
     option: 'Batman',
     votes: 14,
-    color: 'rgb(36, 36, 36)',
+    backgroundColor: 'rgba(36, 36, 36, 0.3)',
+    borderColor: 'rgb(36, 36, 36)',
   },
   {
     option: 'Hulk',
     votes: 3,
-    color: 'rgb(155,192,99)',
+    backgroundColor: 'rgba(155,192,99, 0.3)',
+    borderColor: 'rgb(155,192,99)',
   },
   {
     option: 'Wolverine',
     votes: 7,
-    color: 'rgb(255,180,0)',
+    backgroundColor: 'rgba(255,180,0, 0.3)',
+    borderColor: 'rgb(255,180,0)',
   },
   {
     option: 'Other',
     votes: 10,
-    color: 'rgb(153,102,255)',
+    backgroundColor: 'rgba(153,102,255, 0.3)',
+    borderColor: 'rgb(153,102,255)',
   },
 ];
 
@@ -40,29 +47,14 @@ const ctx = document.getElementById('chart').getContext('2d');
 const pollChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: pollData.map(pollOption => pollOption.option),
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: 'rgba(0, 0, 0, 0.75)',
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        hoverBackgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderWidth: 5,
+        label: 'Number of Votes',
+        data: pollData.map(pollVotes => pollVotes.votes),
+        backgroundColor: pollData.map(bgColor => bgColor.backgroundColor),
+        borderColor: pollData.map(bdrColor => bdrColor.borderColor),
+        borderWidth: 3,
       },
     ],
   },
